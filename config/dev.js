f24ad994291cd57687bc4222f91327888f2b1def
afashs <dev.afashs@gmail.com>
2020-12-28 17:12:31 +0900

    key info hide

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://devmrseo:pass1643@boilerplate.5a1yd.mongodb.net/basic?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,14 +1,35 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://devmrseo:pass1643@boilerplate.5a1yd.mongodb.net/basic?retryWrites=true&w=majority', {
-  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
+const config = require("./config/key");
 
+const { User } = require("./models/user");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get("/", ( req, res ) => res.send("Hello World!! 안녕하세요!"));
+const mongoose = require("mongoose");
+mongoose
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("MongoDB Connected..."))
+  .catch((err) => console.log(err));
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({ success: true });
+  });
+});
+
+app.get("/", (req, res) => res.send("Hello World!! "));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
